5f903319400dae191e0317e782f9e34389aeb71d
andreea1603 <andreeaciocan2000@yahoo.ro>
2021-12-22 14:58:28 +0200

    add history

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ k = 0
 
 def show():
     fig = plt.figure(figsize=(10, 10))
+    makeCsv()
     ani = FuncAnimation(plt.gcf(), animateData, interval=1000)
     plt.tight_layout()
     plt.show()
@@ -113,11 +114,9 @@ def plotButton():
         k = 1
 
         def savePdf():
-            print("am intrat aici")
             plt.savefig('plotTry.pdf')
 
         def savePng():
-            print("am intrat aici")
             plt.savefig('plotTry.png')
 
         b1 = Button(text="Save as Png", command=savePng)
@@ -151,9 +150,9 @@ def animateData(i):
 
     plt.subplot(2, 4, 8)
     plotButton()
-
     plt.plot()
     dataToCsv()
+
 
 def net_usage(inf="Wi-Fi"):
     global indexNet
@@ -181,7 +180,6 @@ def net_usage(inf="Wi-Fi"):
         net_in = round((net_in_2 - net_in_1) / 1024 / 1024, 3)
         net_out = round((net_out_2 - net_out_1) / 1024 / 1024, 3)
         lastNetStat = net_stat
-        # print(f"Current net-usage:\n IIOIOIOIO IN: {net_in * 8 * 1000} MB/s, OUT: {net_out * 8*1000} MB/s")
         return [net_in * 8000, net_out * 8000]
 
 
@@ -218,7 +216,8 @@ def dataToCsv():
     global yNetRecvSpeed, yNetSentSpeed, yDiskSpeedRead, y_val, y_val1, yDiskSpeedWrite
     with open(filePath, 'a') as f:
         writer = csv.writer(f)
-        data =[y_val1[-1], y_val[-1], psutil.disk_usage('/').percent, yDiskSpeedWrite[-1], yDiskSpeedRead[-1],yNetRecvSpeed[-1], yNetSentSpeed[-1]]
+        data = [y_val1[-1], y_val[-1], psutil.disk_usage('/').percent, yDiskSpeedWrite[-1], yDiskSpeedRead[-1],
+                yNetRecvSpeed[-1], yNetSentSpeed[-1]]
         writer.writerow(data)
         f.close()
 
@@ -229,13 +228,14 @@ def makeCsv():
     path = time.strftime("%d") + time.strftime("%m") + time.strftime("%y")
     path = path + "hm" + time.strftime("%H%M")
     completePath = 'history/' + path + '.csv'
-    print(completePath)
     header = ['CPU usage', 'Memory', 'Disk Usage', 'Disk write speed',
               'Disk read speed', 'Wi-Fi speed receive', 'Wi-Fi speed Send']
     filePath = completePath
     with open(completePath, 'w', encoding='UTF8', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(header)
+
+
 def getAvailableData():
     f = []
     filenamesT = tuple()
@@ -243,15 +243,110 @@ def getAvailableData():
         f.extend(filenames)
         filenamesT = filenamesT + tuple(filenames)
     return filenamesT
-def getOptionValue():
-    print("value is:" + variable.get())
-def plotOption():
-    print("value is:" + variable.get())
-    path = 'history/' + variable.get()
-    file = open(path)
-    csvreader = csv.reader(file)
-    for row in csvreader:
-        print("Row", row)
+
+
+class History:
+    xMemory = []
+    xCpu = []
+    xNetSentSpeed = []
+    xNetRecvSpeed = []
+    xDiskSpeedWrite = []
+    xDiskSpeedRead = []
+    xDiskUsage = []
+
+    def clearValues(self):
+        self.xMemory = []
+        self.xCpu = []
+        self.xNetSentSpeed = []
+        self.xNetRecvSpeed = []
+        self.xDiskSpeedWrite = []
+        self.xDiskSpeedRead = []
+        self.xDiskUsage = []
+
+    def plotOption(self):
+        path = 'history/' + variable.get()
+        self.clearValues()
+        file = open(path)
+        csvReader = csv.reader(file)
+        ok = 0
+        for row in csvReader:
+            if len(row) > 1 and ok == 1:
+                self.xCpu.append(float(row[0]))
+                self.xMemory.append(float(row[1]))
+                self.xDiskUsage.append(float(row[2]))
+                self.xDiskSpeedWrite.append(float(row[3]))
+                self.xDiskSpeedRead.append(float(row[4]))
+                self.xNetRecvSpeed.append(float(row[5]))
+                self.xNetSentSpeed.append(float(row[6]))
+            ok = 1
+        self.plotTry()
+    def plotDisk(self):
+        y = np.array([float(self.xDiskUsage[-1]), float(100-self.xDiskUsage[-1])])
+        myLabels = ["Used", "Not used"]
+        plt.title("Disk usage")
+        myExplode = [0.2, 0]
+        plt.pie(y, labels=myLabels, explode=myExplode, shadow=True, autopct='%1.2f%%')
+
+    def plotData(self, data):
+        y = []
+        for i in range(0, len(self.xMemory)):
+            y.append(i)
+        yPoints = np.array(y)
+
+        if data == "Memory":
+            xPoints = np.array(self.xMemory)
+            plt.plot(yPoints, xPoints, color="firebrick", linestyle="--")
+        elif data == "Cpu":
+            xPoints = np.array(self.xCpu)
+            plt.fill_between(yPoints,xPoints, alpha=0.25, color="magenta")
+            plt.plot(yPoints,xPoints, color="magenta")
+        elif data == "NetRec":
+            xPoints = np.array(self.xNetRecvSpeed)
+            plt.plot(yPoints,xPoints, color="darkviolet")
+        elif data == "NetSend":
+            xPoints = np.array(self.xNetSentSpeed)
+            plt.fill_between(yPoints, xPoints, alpha=0.25, color="salmon")
+            plt.plot(yPoints,xPoints, color="magenta", linestyle="--")
+        elif data == "DiskRead":
+            xPoints = np.array(self.xDiskSpeedRead)
+            plt.fill_between(yPoints,xPoints, alpha=0.45, color="dodgerblue")
+            plt.plot(yPoints,xPoints, color="dodgerblue", linestyle="-.")
+        else:
+            xPoints = np.array(self.xDiskSpeedWrite)
+            plt.plot(yPoints, xPoints, color="deeppink", linestyle="-.")
+        plt.title(data)
+        plt.ylabel("percent")
+        plt.xlabel("Time")
+
+    def plotTry(self):
+        fig = plt.figure(figsize=(11, 8))
+        fig.canvas.set_window_title('Setting up window title.')
+        fig.subplots_adjust(bottom=0.020, left=0.20, top=0.900, right=0.800)
+        plt.title(variable.get())
+        plt.subplot(3, 2, 1)
+        self.plotData("Memory")
+
+        plt.subplot(3, 2, 2)
+        self.plotData("Cpu")
+
+        plt.subplot(3, 3, 4)
+        self.plotData("NetRec")
+
+        plt.subplot(3, 3, 5)
+        self.plotData("NetSend")
+
+        plt.subplot(3, 3, 6)
+        self.plotData("DiskRead")
+
+
+        plt.subplot(3, 2, 5)
+        self.plotData("DiscWrite")
+
+        plt.subplot(3, 2, 6)
+        self.plotDisk()
+
+        plt.show()
+
 
 if __name__ == '__main__':
     root = Tk()
@@ -269,8 +364,9 @@ if __name__ == '__main__':
     w.pack()
     w.place(x=100, y=300, width=150)
 
-    button = Button(root, text="View History", command=plotOption)
+    v = History()
+    button = Button(root, text="View History", command=v.plotOption)
     button.pack()
     button.place(x=290, y=300, width=150)
-    makeCsv()
     root.mainloop()
+    # v.plotTry()
